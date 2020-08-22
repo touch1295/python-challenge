a166f84a5eafd759c6c8fbc367a16c1a6eec5fab
touch1295 <69025171+touch1295@users.noreply.github.com>
2020-08-22 03:33:27 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,71 +2,85 @@
 import os
 import csv
 
-# specify the file location 
-input_file = os.path.join("Resources", "budget_data.csv")
+# specify the file location
+input_file = os.path.join("Resources", "election_data.csv")
 
+# Declaring Variables
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
 
-# Creating empty Lists for data
-sum_months = []
-sum_profit = []
-monthly_profit_change = []
+# Open csv in default read mode with context manager
+with open(input_file,newline="", encoding="utf-8") as elections:
 
-with open(input_file, newline="", encoding="utf-8") as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+    # Store data under the csvreader variable
+    csvreader = csv.reader(elections,delimiter=",")
 
-    # Skip header labels to iterate with the values afforded
+    # Skiping header to iterate with the values afforded
     header = next(csvreader)
 
+    # Iterate through each row in the csv
     for row in csvreader:
-        # Appending total months & total profit to lists respectively
-        sum_months.append(row[0])
-        sum_profit.append(int(row[1]))
 
-    # Iterate through profits to derive the monthly change in profits
-    for i in range(len(sum_profit)-1):
+        # Counting total numbers of votes.
+        total_votes +=1
 
-        # Finding differences between two months and append to monthly profit change
-        monthly_profit_change.append(sum_profit[i+1]-sum_profit[i])
+        if row[2] == "Khan":
+            khan_votes +=1
+        elif row[2] == "Correy":
+            correy_votes +=1
+        elif row[2] == "Li":
+            li_votes +=1
+        elif row[2] == "O'Tooley":
+            otooley_votes +=1
 
-# Derive the max and min of the the monthly profit change list
-max_increase_value = max(monthly_profit_change)
-max_decrease_value = min(monthly_profit_change)
+ # making a dictionary out of the two lists previously created then the winner is disovered
+candidates = ["Khan", "Correy", "Li","O'Tooley"]
+votes = [khan_votes, correy_votes,li_votes,otooley_votes]
+
+# Zip lists together based candidate(key) and the total votes(value)
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
+# Printing summary of the votes
+khan_percent = (khan_votes/total_votes) *100
+correy_percent = (correy_votes/total_votes) * 100
+li_percent = (li_votes/total_votes)* 100
+otooley_percent = (otooley_votes/total_votes) * 100
+
+# Printing summary table
+print(f"Election Results")
+print(f"----------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"----------------------------")
+print(f"Khan: {khan_percent:.3f}% ({khan_votes})")
+print(f"Correy: {correy_percent:.3f}% ({correy_votes})")
+print(f"Li: {li_percent:.3f}% ({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})")
+print(f"----------------------------")
+print(f"Winner: {key}")
+print(f"----------------------------")
 
 
-# The + 1 at the end == month associated with change is the next month or +1 month
-max_increase_month = monthly_profit_change.index(
-    max(monthly_profit_change)) + 1
-max_decrease_month = monthly_profit_change.index(
-    min(monthly_profit_change)) + 1
-
-# Printing Statements
-
-print("Financial Analysis")
-print("----------------------------")
-print(f"Total Months: {len(sum_months)}")
-print(f"Total: ${sum(sum_profit)}")
-print(
-    f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}")
-print(
-    f"Greatest Increase in Profits: {sum_months[max_increase_month]} (${(str(max_increase_value))})")
-print(
-    f"Greatest Decrease in Profits: {sum_months[max_decrease_month]} (${(str(max_decrease_value))})")
-
-# Set variable for output file
-output_file = os.path.join("text-file-analysis", "Financial_Analysis.txt")
+# Setting variables for output file
+output_file = os.path.join("text-file-analysis","Election_Results.txt")
 
 #  Open the output file
-with open(output_file, "w") as file:
+with open(output_file,"w") as file:
 
-    # Write in zipped rows
-    # Write methods to print to Financial_Analysis
-    file.write("Financial Analysis \n")
-    file.write("---------------------------- \n")
-    file.write(f"Total Months: {len(sum_months)}\n")
-    file.write(f"Total: ${sum(sum_profit)}\n")
-    file.write(
-        f"Average Change: {round(sum(monthly_profit_change)/len(monthly_profit_change),2)}\n")
-    file.write(
-        f"Greatest Increase in Profits: {sum_months[max_increase_month]} (${(str(max_increase_value))})\n")
-    file.write(
-        f"Greatest Decrease in Profits: {sum_months[max_decrease_month]} (${(str(max_decrease_value))})\n")
+# Writing in zipped by row
+# Write fxn to print Elections_Results_Summary
+    file.write(f"Election Results\n")
+    file.write(f"----------------------------\n")
+    file.write(f"Total Votes: {total_votes}\n")
+    file.write(f"----------------------------\n")
+    file.write(f"Khan: {khan_percent:.3f}% ({khan_votes})\n")
+    file.write(f"Correy: {correy_percent:.3f}% ({correy_votes})\n")
+    file.write(f"Li: {li_percent:.3f}% ({li_votes})\n")
+    file.write(f"O'Tooley: {otooley_percent:.3f}% ({otooley_votes})\n")
+    file.write(f"----------------------------\n")
+    file.write(f"Winner: {key}\n")
+    file.write(f"----------------------------\n")
+

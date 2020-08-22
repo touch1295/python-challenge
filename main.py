5c467634e89712a2410173df4f2ae77a95c3fc65
touch1295 <69025171+touch1295@users.noreply.github.com>
2020-08-22 02:39:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-# Import Dependencies
+# Dependency modules for reading
 import os
 import csv
 
-# Declare file location through pathlib
+# specify the file location 
 input_file = os.path.join("Resources", "budget_data.csv")
 
 
-# Create empty Lists to store data
+# Creating empty Lists for data
 sum_months = []
 sum_profit = []
 monthly_profit_change = []
@@ -14,32 +14,32 @@ monthly_profit_change = []
 with open(input_file, newline="", encoding="utf-8") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    # Skip the header labels to iterate with the values
+    # Skip header labels to iterate with the values afforded
     header = next(csvreader)
 
     for row in csvreader:
-        # Append the total months and total profit to their corresponding lists
+        # Appending total months & total profit to lists respectively
         sum_months.append(row[0])
         sum_profit.append(int(row[1]))
 
-    # Iterate through the profits in order to get the monthly change in profits
+    # Iterate through profits to derive the monthly change in profits
     for i in range(len(sum_profit)-1):
 
-        # Take the difference between two months and append to monthly profit change
+        # Finding differences between two months and append to monthly profit change
         monthly_profit_change.append(sum_profit[i+1]-sum_profit[i])
 
-# Obtain the max and min of the the monthly profit change list
+# Derive the max and min of the the monthly profit change list
 max_increase_value = max(monthly_profit_change)
 max_decrease_value = min(monthly_profit_change)
 
 
-# We use the plus 1 at the end since month associated with change is the + 1 month or next month
+# The + 1 at the end == month associated with change is the next month or +1 month
 max_increase_month = monthly_profit_change.index(
     max(monthly_profit_change)) + 1
 max_decrease_month = monthly_profit_change.index(
     min(monthly_profit_change)) + 1
 
-# Print Statements
+# Printing Statements
 
 print("Financial Analysis")
 print("----------------------------")
